6508661ae95d44218ab79061467a89665bd3a145
b1ackd0t <28790446+rodneyosodo@users.noreply.github.com>
2024-07-13 23:52:37 +0300

    👷 Add Continuous Integration Pipeline (#2)
    * chore(ci): Add dependabot
    
    * chore(ci): Add Continuous Integration Pipeline
    
    Signed-off-by: Rodney Osodo <socials@rodneyosodo.com>
    
    ---------
    
    Signed-off-by: Rodney Osodo <socials@rodneyosodo.com>

--- src/wakatime.rs
+++ src/wakatime.rs
@@ -22,10 +22,10 @@ impl WakatimeExtension {
         }
 
         zed::set_language_server_installation_status(
-            &language_server_id,
+            language_server_id,
             &zed_extension_api::LanguageServerInstallationStatus::CheckingForUpdate,
         );
-        
+
         let release = zed::latest_github_release(
             "mrnossiom/wakatime-lsp",
             zed::GithubReleaseOptions {
@@ -60,12 +60,15 @@ impl WakatimeExtension {
             .ok_or_else(|| format!("no asset found matching {:?}", asset_name))?;
 
         let version_dir = format!("wakatime-lsp-{}", release.version);
-        let asset_name = asset_name.split('.').next().expect("failed to split asset name");
+        let asset_name = asset_name
+            .split('.')
+            .next()
+            .expect("failed to split asset name");
         let binary_path: String = format!("{version_dir}/{asset_name}/wakatime-lsp");
 
         if !fs::metadata(&binary_path).map_or(false, |stat| stat.is_file()) {
             zed::set_language_server_installation_status(
-                &language_server_id,
+                language_server_id,
                 &zed::LanguageServerInstallationStatus::Downloading,
             );
 
@@ -86,7 +89,7 @@ impl WakatimeExtension {
             for entry in entries {
                 let entry = entry.map_err(|e| format!("failed to load directory entry {e}"))?;
                 if entry.file_name().to_str() != Some(&version_dir) {
-                    fs::remove_dir_all(&entry.path()).ok();
+                    fs::remove_dir_all(entry.path()).ok();
                 }
             }
         }
